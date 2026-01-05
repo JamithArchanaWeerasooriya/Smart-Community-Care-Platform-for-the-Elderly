dbaeac58cabe6cd079e4969d87a0b17f88ddd495
Savindu <msavindu10@gmail.com>
2026-01-05 16:23:05 +0530

    Update install.bat

--- rasa/install.bat
+++ rasa/install.bat
@@ -1,8 +1,14 @@
 @echo off
 
 echo Installing (Savindu Parts(Rasa))...
-start cmd /c "py -m venv venv && venv\Scripts\activate & pip install --upgrade pip & pip install flask flask-cors rasa==3.6.20"
+
+call py -m venv venv
+call venv\Scripts\activate
+call pip install --upgrade pip
+call pip install flask flask-cors rasa==3.6.20
 
 echo ===============================
 echo Installed successfully
 echo ===============================
+
+pause
