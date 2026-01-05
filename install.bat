3ae3fa71a2ae12ddea304497af9e5a61d9a8006e
Savindu <msavindu10@gmail.com>
2026-01-05 12:17:58 +0530

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,10 +4,10 @@ echo Installing(Frontend)...
 start cmd /c "cd frontend && npm install"
 
 echo Installing(Backend)...
-start cmd /c "cd backend && npm install"
+start cmd /c "cd backend && npm i -g nodemon && npm install"
 
 echo Installing (Savindu Parts(Rasa))...
-start cmd /c "cd rasa && py -m venv venv && venv\Scripts\activate & pip install --upgrade pip & pip install flask flask-cors rasa==3.6.20"
+start cmd /k "cd rasa && py -m venv venv && venv\Scripts\activate & pip install --upgrade pip && pip install flask flask-cors rasa==3.6.20"
 
 echo ===============================
 echo Installed successfully
